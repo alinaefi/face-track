748e7d6284a6e0c0277691d062e8b6a0933a17a4
alinaefi <efimovaalina2011@gmail.com>
2025-03-17 09:47:06 +0000

    define repo task interface

--- internal/pkg/repo/repo.go
+++ internal/pkg/repo/repo.go
@@ -1,7 +1,9 @@
 package repo
 
 import (
+	"face-track/internal/pkg/model"
 	"face-track/internal/pkg/repo/task_repo"
+	"image"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -17,4 +19,16 @@ func NewRepo(db *sqlx.DB) *Repo {
 }
 
 type Task interface {
+	GetTaskById(taskId int) (taskRow *model.Task, err error)
+	GetTaskImages(taskId int) (images []*model.Image, err error)
+	GetFacesByImageIds(imageIds []int) (taskFaces map[int][]*model.Face, err error)
+	CreateTask() (taskId int, err error)
+	DeleteTask(taskId int) (err error)
+	SaveImageDisk(taskId int, image image.Image, imageName string) (imageRow *model.Image, err error)
+	DecodeFile(fileData *model.FileData) (img image.Image, err error)
+	UpdateTaskStatus(taskId int, status string) (err error)
+	GetFaceDetectionData(image *model.Image, token string) (imageData *model.FaceCloudDetectResponse, err error)
+	GetFaceCloudToken() (token string, err error)
+	SaveProcessedData(processedFaces []*model.Face, processedImages []*model.Image)
+	UpdateTaskStatistics(task *model.Task) (err error)
 }
