e88dceaf09ee843c2845afa970c5b217365f2c70
alinaefi <efimovaalina2011@gmail.com>
2025-03-18 13:20:16 +0000

    documented package handler

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -1,3 +1,4 @@
+// Package handler provides HTTP server setup and request handling for the application.
 package handler
 
 import (
@@ -10,20 +11,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const (
-	serverAddrName = "FACE_TRACK__SERVER_ADDRESS"
-)
+// serverAddrName is an env variable key for the Face Track server address.
+const serverAddrName = "FACE_TRACK__SERVER_ADDRESS"
 
+// Handler is responsible for handling incoming HTTP requests and routing them
+// to the appropriate service methods.
 type Handler struct {
 	service *service.Service
 }
 
+// NewHandler returns a new Handler instance.
 func NewHandler(service *service.Service) *Handler {
 	return &Handler{
 		service: service,
 	}
 }
 
+// NewServer initializes and returns an HTTP server.
 func NewServer(s *service.Service) *http.Server {
 	tools.CheckEnvs(serverAddrName)
 
